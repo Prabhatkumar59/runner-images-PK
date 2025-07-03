3f6cb666f958902bb9532a150bceff32367ed0ce
Prabhat kumar <prabhatkumar59@github.com>
2025-07-03 11:48:37 +0530

    Skipping Cosmos DB Emulator install on Windows Server 2025 – not supported

--- images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
+++ images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
@@ -3,6 +3,13 @@
 ##  Desc:  Install Azure CosmosDb Emulator
 ####################################################################################
 
+# Skip install and test on Windows Server 2025
+$osVersion = (Get-CimInstance Win32_OperatingSystem).Version
+if ($osVersion -like "10.0.25*") {
+    Write-Host "Skipping Cosmos DB Emulator install on Windows Server 2025 – not supported."
+    return
+}
+
 Install-Binary -Type MSI `
     -Url "https://aka.ms/cosmosdb-emulator"
 
